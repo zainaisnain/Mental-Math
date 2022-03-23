37a092d187a870c5cd79bd3f671a689856b99c5f
Zaina Isnain <zainaisnain@gmail.com>
2022-03-24 03:34:58 +0800

    sample commit n push

--- app/src/main/java/com/example/mentalmath/Addition.kt
+++ app/src/main/java/com/example/mentalmath/Addition.kt
@@ -1,11 +1,33 @@
 package com.example.mentalmath
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Addition : AppCompatActivity() {
+
+    private var correctAnswer: Int = 0
+    private var equation: String = ""
+    private var firstnum = 0
+    private var secondnum = 0
+    // Stats
+    private var num_of_correct = 0
+    private var wrong = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addition)
+
+
+        firstnum = (1..11).random()
+        secondnum = (1..11).random()
+
+        equation = "$firstnum + $secondnum"
+
+        var question = findViewById<TextView>(R.id.question)
+        question.text = equation
     }
 }
